2267e599f3e9b3da8c265a85c4118fa86914dc71
Rasmus Soome <rsoome16@gmail.com>
2016-11-27 23:24:39 +0200

    Testing ball locating and speed calibration

--- src/rtex.cpp
+++ src/rtex.cpp
@@ -8,15 +8,31 @@
 #include "tuum_motion.hpp"
 #include "tuum_context.hpp"
 
+#include "tuum_visioning.hpp"
+#include "tuum_localization.hpp"
+#include "tuum_navigation.hpp"
+#include "tuum_motion.hpp"
+#include "tuum_context.hpp"
+
+#include "hal.hpp"
+
+
 using namespace tuum;
 
 namespace rtex {
+
 
   STM* stm;
   //test
 
   void setup() {
-    tuum::gMotion->setAimTarget({10, 10});
+
+    Ball* b = nullptr;
+    if(b != nullptr){
+      b = gNavigation->getNearestBall();
+      vec2i pos = gNavigation->calcBallPickupPos(b->getTransform()).getPosition();
+      tuum::gMotion->setAimTarget(pos);
+    }
   }
 
   void process() {
