1544dd87d3a4cbe6e6126f4190ac6852d6d519f3
Meelik Kiik <kiik.meelik@gmail.com>
2017-11-10 17:01:35 +0200

    Add global context files.

---
+++ inc/rtx_context.hpp
@@ -0,0 +1,11 @@
+
+#ifndef RTX_CONTEXT_H
+#define RTX_CONTEXT_H
+
+namespace tuum {
+  class GameField;
+
+  extern GameField* gGameField;
+}
+
+#endif
---
+++ src/rtx_context.cpp
@@ -0,0 +1,8 @@
+
+#include "rtx_GameField.hpp"
+
+#include "rtx_context.hpp"
+
+namespace tuum {
+  GameField *gGameField = nullptr;
+}
